ca442f398b0416da74877f813f3536899bfd5e04
Brian Smith <brian.bubbam@gmail.com>
2021-06-16 17:11:26 -0500

    commit tree c2be7df74e4b9c8cc7b1d7cbf33d41d7581394d4

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,28 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child(props) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+function Child({ onChangeColor, color }) {
+  console.log(onChangeColor);
+
+  function handleClick() {
+    const newColor = getRandomColor();
+    onChangeColor(newColor); // created a function and then called the
+                    //prop as a call back to pass it back to the parent
+  }
+
+
+
+
+
+  return (
+      <div
+        onClick={handleClick}
+        className="child"
+        style={{ backgroundColor: color }}
+        
+      />
+
+  ) 
 }
 
 export default Child;
